0ca883ad46ffbaa68fe5eb94b5580abc5d24bb9c
www223-arch <3577454701@qq.com>
2026-02-02 04:22:45 +0800

    添加ws2812组件（spi/rmt），大概理清楚SPI发送的逻辑

--- Fly_Idf/idf_test/esp_idf001/main/main.c
+++ Fly_Idf/idf_test/esp_idf001/main/main.c
@@ -1,78 +1,89 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Unlicense OR CC0-1.0
+ */
 #include <stdio.h>
-#include "driver/gptimer.h"
-#include "driver/ledc.h"
 #include "freertos/FreeRTOS.h"
-void  ws2812_demo_task(void *arg);
+#include "freertos/task.h"
+#include "led_strip.h"
+#include "esp_log.h"
+#include "esp_err.h"
 
-// ====================== 硬件配置 ======================
-void app_main(void)
+// Set to 1 to use DMA for driving the LED strip, 0 otherwise
+// Please note the RMT DMA feature is only available on chips e.g. ESP32-S3/P4
+#define LED_STRIP_USE_DMA  1
+
+#if LED_STRIP_USE_DMA
+// Numbers of the LED in the strip
+#define LED_STRIP_LED_COUNT 256
+#define LED_STRIP_MEMORY_BLOCK_WORDS 1024 // this determines the DMA block size
+#else
+// Numbers of the LED in the strip
+#define LED_STRIP_LED_COUNT 1
+#define LED_STRIP_MEMORY_BLOCK_WORDS 0 // let the driver choose a proper memory block size automatically
+#endif // LED_STRIP_USE_DMA
+
+// GPIO assignment
+#define LED_STRIP_GPIO_PIN  48
+
+// 10MHz resolution, 1 tick = 0.1us (led strip needs a high resolution)
+#define LED_STRIP_RMT_RES_HZ  (10 * 1000 * 1000)
+
+static const char *TAG = "example";
+
+led_strip_handle_t configure_led(void)
 {
+    // LED strip general initialization, according to your led board design
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = LED_STRIP_GPIO_PIN, // The GPIO that connected to the LED strip's data line
+        .max_leds = LED_STRIP_LED_COUNT,      // The number of LEDs in the strip,
+        .led_model = LED_MODEL_WS2812,        // LED strip model
+        .color_component_format = LED_STRIP_COLOR_COMPONENT_FMT_GRB, // The color order of the strip: GRB
+        .flags = {
+            .invert_out = false, // don't invert the output signal
+        }
+    };
 
-    // // 2. 创建演示任务（栈大小4096足够）
-    xTaskCreate(ws2812_demo_task, "ws2812_demo", 4096, NULL, 5, NULL);
+    // LED strip backend configuration: RMT
+    led_strip_rmt_config_t rmt_config = {
+        .clk_src = RMT_CLK_SRC_DEFAULT,        // different clock source can lead to different power consumption
+        .resolution_hz = LED_STRIP_RMT_RES_HZ, // RMT counter clock frequency
+        .mem_block_symbols = LED_STRIP_MEMORY_BLOCK_WORDS, // the memory block size used by the RMT channel
+        .flags = {
+            .with_dma = LED_STRIP_USE_DMA,     // Using DMA can improve performance when driving more LEDs
+        }
+    };
 
+    // LED Strip object handle
+    led_strip_handle_t led_strip;
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+    ESP_LOGI(TAG, "Created LED strip object with RMT backend");
+    return led_strip;
 }
 
-void  ws2812_demo_task(void *arg)
+void app_main(void)
 {
-ledc_timer_config_t ledc_timer = {
-        .speed_mode       = LEDC_LOW_SPEED_MODE,
-        .timer_num        = LEDC_TIMER_0,
-        .duty_resolution  = LEDC_TIMER_13_BIT,
-        .freq_hz          = 5000,  // Frequency in Hertz. Set frequency at 5 kHz
-        .clk_cfg          = LEDC_AUTO_CLK
-    };
-    // Set configuration of timer0 for high speed channels
-    ledc_timer_config(&ledc_timer);
+    led_strip_handle_t led_strip = configure_led();
+    bool led_on_off = true;
 
-    ledc_channel_config_t ledc_channel = {
-        .speed_mode     = LEDC_LOW_SPEED_MODE,
-        .channel        = LEDC_CHANNEL_0,
-        .timer_sel      = LEDC_TIMER_0,
-        .intr_type      = LEDC_INTR_DISABLE,
-        .gpio_num       = 16,
-        .duty           = 4096, // Set duty to 50%
-        .hpoint         = 0
-    };
-        ledc_channel_config(&ledc_channel);
-        ledc_channel.channel = LEDC_CHANNEL_1;
-        ledc_channel.gpio_num       = 17;
-        ledc_channel_config(&ledc_channel);
+    ESP_LOGI(TAG, "Start blinking LED strip");
+    while (1) {
+        if (led_on_off) {
+            /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
+            for (int i = 0; i < LED_STRIP_LED_COUNT; i++) {
+                ESP_ERROR_CHECK(led_strip_set_pixel(led_strip, i, 5, 0, 0));
+            }
+            /* Refresh the strip to send data */
+            ESP_ERROR_CHECK(led_strip_refresh(led_strip));
+            ESP_LOGI(TAG, "LED ON!");
+        } else {
+            /* Set all LED off to clear all pixels */
+            ESP_ERROR_CHECK(led_strip_clear(led_strip));
+            ESP_LOGI(TAG, "LED OFF!");
+        }
 
-        ledc_channel.channel = LEDC_CHANNEL_2;
-        ledc_channel.gpio_num   = 18;
-        ledc_channel_config(&ledc_channel);
-   
-        ledc_channel.channel = LEDC_CHANNEL_3;
-        ledc_channel.gpio_num   = 16;
-        ledc_channel_config(&ledc_channel);
-
-        ledc_channel.channel = LEDC_CHANNEL_4;
-        ledc_channel.gpio_num   = 4;
-        ledc_channel_config(&ledc_channel);
-
-        ledc_channel.channel = LEDC_CHANNEL_5;
-        ledc_channel.gpio_num   = 5;
-        ledc_channel_config(&ledc_channel);
-
-        ledc_channel.channel = LEDC_CHANNEL_6;
-        ledc_channel.gpio_num   = 6;
-        ledc_channel.duty= 4096*2/5; // Set duty to 50%
-
-        ledc_channel_config(&ledc_channel);
-
-        ledc_channel.channel = LEDC_CHANNEL_7;
-        ledc_channel.gpio_num   = 7;
-        ledc_channel.duty= 4096*2/3; // Set duty to 50%
-
-        ledc_channel_config(&ledc_channel);
-
-        ledc_channel.channel = LEDC_CHANNEL_0;
-        ledc_channel.gpio_num   = 15;
-        ledc_channel.duty           = 4096*2/4; // Set duty to 50%
-        ledc_channel_config(&ledc_channel);
-
-        while(1) {
-         vTaskDelay(500 / portTICK_PERIOD_MS);
+      //  led_on_off = !led_on_off;
+        vTaskDelay(pdMS_TO_TICKS(500));
     }
 }
